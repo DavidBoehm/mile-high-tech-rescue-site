9991f9c807eaf519225ef1c06e830130829dc13a
David Boehm <thedavidboehm@gmail.com>
2026-04-23 09:10:44 -0600

    Remove technerdhq - now standalone repo at github.com/DavidBoehm/technerdhq

--- technerdhq/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'TechNerdHQ';
-export const SITE_DESCRIPTION = 'Practical tech tips for home networks, WiFi, smart homes, and troubleshooting. Serving Littleton, CO and beyond.';
